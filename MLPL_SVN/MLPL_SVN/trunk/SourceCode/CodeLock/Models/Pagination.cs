9a8c4ce3ee6605ac13e70fcb2c99327b8c958a17
Rahul <rahuljangra807@gmail.com>
2024-07-18 21:00:33 +0530

    fix pagination issue

--- MLPL_SVN/MLPL_SVN/trunk/SourceCode/CodeLock/Models/Pagination.cs
+++ MLPL_SVN/MLPL_SVN/trunk/SourceCode/CodeLock/Models/Pagination.cs
@@ -6,65 +6,16 @@ using System.Threading.Tasks;
 
 namespace CodeLock.Models
 {
-   
-
-    public class PaginationData
-    {
-        public int start { get; set; }
-        public int length { get; set; }
-        public Search search { get; set; }
-        public List<Column> columns { get; set; }
-        public List<Order> order { get; set; }
-    }
-
-    public class Search
-    {
-        public string value { get; set; }
-        public string fromDate { get; set; }
-        public string toDate { get; set; }
-        public string level { get; set; }
-        public string levelType { get; set; }
-    }
-
-    public class Column
-    {
-        public string name { get; set; }
-    }
-
-    //public class Order
-    //{
-    //    public int column { get; set; }
-    //    public string dir { get; set; }
-    //}
-
-
-
     public class Pagination
     {
         public DTPostData data { get; set; }
-        public  DateTime toDate { get; set; }
-        public short levelType { get; set; }
-        public short level { get; set; }
-        public DateTime    fromDate { get; set; }
-        public int length { get; set; }
-        public int start { get; set; }
-        public string sorting { get; set; }
-        public PaginationData Data { get; set; }
+        public int TotalVehicle { get; set; }
+        public int FilterVehcile { get; set; }
 
-       public string documentNo { get; set; }
-            public string manualDocumentNo { get; set; }
-            public int customerId { get; set; }
     }
-  
+
     public class DTPostData
     {
-        public   DateTime toDate { get; set; }
-        public  int levelType { get; set; }
-        public int level { get; set; }
-        public DateTime fromDate { get; set; }
-
-        public short ToLocationId;
-        public short FromLocationId;
         public int draw { get; set; }
         public int start { get; set; }
         public int length { get; set; }
@@ -72,7 +23,7 @@ namespace CodeLock.Models
         public DTSearch search { get; set; }
         public List<DTOrder> order { get; set; }
     }
-    
+
     public class DTColumn
     {
         public string data { get; set; }
@@ -84,12 +35,6 @@ namespace CodeLock.Models
 
     public class DTSearch
     {
-        public string fromDate;
-        public string toDate;
-        public string level;
-        public string levelType;
-        public short ToLocationId;
-        public short FromLocationId;
         public string value { get; set; }
         public string regex { get; set; }
     }
@@ -98,13 +43,15 @@ namespace CodeLock.Models
     {
         public int column { get; set; }
         public string dir { get; set; }
-        public int Length { get; set; }
     }
     public class DTResponse
     {
         public int recordsTotal { get; set; }
         public int recordsFiltered { get; set; }
-        public object data { get; set; }
-     
+        public string data { get; set; }
+        public int TotalVehicle { get; set; }
+        public int FilterVehcile { get; set; }
+        public string FilterChasisNo { get; set; }
+
     }
 }
